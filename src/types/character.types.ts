d1bacbe6903d663571f649047e0679443fb2d911
Marlee Garcia <marlee.garcia121@gmail.com>
2022-09-15 19:57:19 -0400

    Adding types

---
+++ src/types/character.types.ts
@@ -0,0 +1,7 @@
+export interface Character {
+	_id: string;
+	userId: string;
+	characterName: string;
+	createdAt: string;
+	updatedAt: string;
+}
---
+++ src/types/link.types.ts
@@ -0,0 +1,8 @@
+export interface Link {
+	_id: string;
+	userId: string;
+	characterId: string;
+	characterName: string;
+	title: string;
+	url: string;
+}
