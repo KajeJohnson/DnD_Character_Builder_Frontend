22fd1b883a6017e674229f0e048c4e80ec9aacf1
Ken Cargill <ken.0.cargill@gmail.com>
2022-09-15 20:59:14 -0400

    details added

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -5,6 +5,17 @@ export type Race = {
     index: string;
     name: string;
     url: string; 
+    speed?: number;
+    ability_bonuses?: [];
+    alignment?: string;
+    age?: string;
+    size?: string;
+    size_description?: string;
+    starting_proficiencies?: string;
+    languages?: [];
+    language_desc?: string;
+    traits?: [];
+    subraces?: [];
 }
 
 export type Races = {
@@ -19,6 +30,46 @@ export function fetchRaces() {
       .then((response) => response.data.results);
 }
 
+export function fetchRace(race: Race) {
+    return axios
+      .get<Race>(
+        `https://www.dnd5eapi.co/api/races/${race.name}`
+      )
+      .then((response) => response.data);
+}
+
+interface RaceProps {
+    item: Race;
+}
+
+export function RaceDetails ({item}: RaceProps) {
+    const [race, setRace] = useState<Race>();
+
+    useEffect(() => {
+        fetchRace(item).then((raceItem) => {
+            console.log(raceItem);
+            setRace(raceItem);
+        });
+    })
+
+    return (
+        <div style={{border: '2px solid #000'}}>
+            <h1>{race?.name}</h1>
+            <p>{race?.alignment}</p>
+            <p>{race?.speed}</p>
+            <p>{race?.ability_bonuses}</p>
+            <p>{race?.alignment}</p>
+            <p>{race?.age}</p>
+            <p>{race?.size}</p>
+            <p>{race?.size_description}</p>
+            <p>{race?.starting_proficiencies}</p>
+            <p>{race?.languages}</p>
+            <p>{race?.language_desc}</p>
+            <p>{race?.traits}</p>
+            <p>{race?.subraces}</p>
+        </div>
+    )
+}
 
 export default function TestRaceList () {
     const [races, setRaces] = useState<Race[]>([]);
@@ -33,9 +84,9 @@ export default function TestRaceList () {
 
 
     return (
-        <div className="MovieList">
+      <div className="MovieList">
         {races.map((race) => (
-          <div>{race.name}</div>
+          <RaceDetails item={race} />
         ))}
       </div>
     )
---
+++ src/types/characterOptions/race.types.tsx
@@ -0,0 +1,9 @@
+export type Race = {
+    index: string;
+    name: string;
+    url: string; 
+}
+
+export type Races = {
+    results: Race[];
+  };
