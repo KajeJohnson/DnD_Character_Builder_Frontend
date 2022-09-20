43fe08229d3efdb86a7b15200dbd7e4778ff5039
Ken Cargill <ken.0.cargill@gmail.com>
2022-09-19 21:25:55 -0400

    stat page free of errors for testing

--- src/pages/StatPage.tsx
+++ src/pages/StatPage.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react"
 import { Character } from "../types/character.types"
+
 export default function StatPage () {
-    
+    const [character, setCharacter] = useState<Character>();
+
 
     return (
         <div>
-            <h3>{Character}</h3>
+
+                    <h3>{character?.characterName}</h3>
+                                
         </div>
     )
 
