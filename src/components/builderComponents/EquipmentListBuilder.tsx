802eaa267321cd1bf796d28f87120de76ed28b47
KJ Johnson <kjohns95@emich.edu>
2022-10-02 19:18:26 -0400

    added notes in equip builder

--- src/components/builderComponents/EquipmentListBuilder.tsx
+++ src/components/builderComponents/EquipmentListBuilder.tsx
@@ -3,83 +3,92 @@ import { fetchEquipments } from "../../services/builderComponents.service";
 import { EquipmentType } from "../../types/characterOptions/equipment.types";
 
 interface Props {
-    onChange: (equipments: EquipmentType[]) => void;
+  onChange: (equipments: EquipmentType[]) => void;
 }
 
-export default function EquipmentListBuilder ({ onChange }: Props) {
-    const [equipmentOps, setEquipmentOps] = useState<EquipmentType[]>([]);
-    const [charEquips, setCharEquips] = useState<EquipmentType[]>([]);
-    const [progress, setProgress] = useState(0);
+export default function EquipmentListBuilder({ onChange }: Props) {
+  const [equipmentOps, setEquipmentOps] = useState<EquipmentType[]>([]);
+  const [charEquips, setCharEquips] = useState<EquipmentType[]>([]);
+  const [progress, setProgress] = useState(0);
 
-    // how to set the charEquips to be added to a player's character data?
+  // how to set the charEquips to be added to a player's character data?
 
+  useEffect(() => {
+    fetchEquipments().then((equipments) => {
+      setEquipmentOps(equipments);
+    });
+  }, []);
 
-    useEffect(() => {
-        fetchEquipments().then((equipments) => {
-          setEquipmentOps(equipments);
-        });
-      }, []);
-
-    function addEquip(selectedEquipment: EquipmentType) {
-        const equipAlreadyChosen = charEquips.find((equipment) => equipment.name === selectedEquipment.name);
-        if (!equipAlreadyChosen) {
-        setCharEquips([selectedEquipment, ...charEquips]);
-        }
+  function addEquip(selectedEquipment: EquipmentType) {
+    const equipAlreadyChosen = charEquips.find(
+      (equipment) => equipment.name === selectedEquipment.name
+    );
+    if (!equipAlreadyChosen) {
+      setCharEquips([selectedEquipment, ...charEquips]);
+      console.log("addequip", selectedEquipment);
+      console.log("equiparray", charEquips);
     }
+  }
 
-    function removeEquip(name: string) {
-        const index = charEquips.findIndex((equipment) => equipment.name === name);
-        let newArray = charEquips.slice(0)
-        newArray.splice(index, 1)
-        setCharEquips(newArray);
-    }
+  function removeEquip(name: string) {
+    const index = charEquips.findIndex((equipment) => equipment.name === name);
+    let newArray = charEquips.slice(0);
+    newArray.splice(index, 1);
+    setCharEquips(newArray);
+  }
 
-    const scrollHandler = (event: React.UIEvent<HTMLDivElement>) => {
-        const containerHeight = event.currentTarget.clientHeight;
-        const scrollHeight = event.currentTarget.scrollHeight;
-    
-        const scrollTop = event.currentTarget.scrollTop;
-        setProgress(((scrollTop + containerHeight) / scrollHeight) * 100);
-      };
+  const scrollHandler = (event: React.UIEvent<HTMLDivElement>) => {
+    const containerHeight = event.currentTarget.clientHeight;
+    const scrollHeight = event.currentTarget.scrollHeight;
 
-      return (
-        <div>
-            <h3> Equipment </h3>
-            <div style={styles.container} onScroll={scrollHandler}>
-          {equipmentOps.map((equipment) => (
-              <div key={equipment.index} style={styles.item}>
+    const scrollTop = event.currentTarget.scrollTop;
+    setProgress(((scrollTop + containerHeight) / scrollHeight) * 100);
+  };
 
-                    {equipment.name}
+  return (
+    <div>
+      <h3> Equipment </h3>
+      <div style={styles.container} onScroll={scrollHandler}>
+        {equipmentOps.map((equipment) => (
+          <div key={equipment.index} style={styles.item}>
+            {equipment.name}
 
-                    <button onClick={() => addEquip(equipment)} type='button'>
-                        add equipment
-                    </button>
+            <button onClick={() => addEquip(equipment)} type="button">
+              add equipment
+            </button>
 
-                    <button onClick={() => removeEquip(equipment.name)} type='button'>
-                        remove  equipment
-                    </button>
-            </div>
-          ))}
-            </div>
-            <button onClick={() => {onChange(charEquips as EquipmentType[])}} type='button'>add selected equipment to character</button>
-        </div>
-      );
+            <button onClick={() => removeEquip(equipment.name)} type="button">
+              remove equipment
+            </button>
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={() => {
+          onChange(charEquips as EquipmentType[]);
+        }}
+        type="button"
+      >
+        add selected equipment to character
+      </button>
+    </div>
+  );
 }
 
 // Styling
 const styles = {
-    container: {
-        width: 500,
-        height: 400,
-        margin: "30px auto",
-        overflowY: "auto",
-        overflowX: "hidden",
-    },
-    item: {
-        margin: "20px 25px",
-        padding: "30px 20px",
-        boxShadow: "0 2px 4px #999",
-        fontSize: "18px",
-        textAlign: "center",
-    }
+  container: {
+    width: 500,
+    height: 400,
+    margin: "30px auto",
+    overflowY: "auto",
+    overflowX: "hidden",
+  },
+  item: {
+    margin: "20px 25px",
+    padding: "30px 20px",
+    boxShadow: "0 2px 4px #999",
+    fontSize: "18px",
+    textAlign: "center",
+  },
 } as const;
