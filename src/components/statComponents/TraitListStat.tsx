8ea3e1f80b25b91bcaa0fcdfc8fd474afecc8242
KJ Johnson <kjohns95@emich.edu>
2022-10-03 18:47:12 -0400

    console.logs in components

--- src/components/statComponents/TraitListStat.tsx
+++ src/components/statComponents/TraitListStat.tsx
@@ -3,38 +3,35 @@ import { fetchTrait } from "../../services/statPage.service";
 import { Trait } from "../../types/characterOptions/traits.types";
 
 interface TraitProps {
-    traits: Trait[];
+  traits: Trait[];
 }
 
-export default function TraitListStatPage ({ traits }: TraitProps) {
-    const [selectedTraits, setSelectedTraits] = useState<Trait[]>([]);
+export default function TraitListStatPage({ traits }: TraitProps) {
+  const [selectedTraits, setSelectedTraits] = useState<Trait[]>([]);
 
-            useEffect(() => {
-                for (const trait of traits) {
-                    fetchTrait(trait.index).then((trait) => {
-                        // console.log('after .then: ' + JSON.stringify(trait));
-                        setSelectedTraits([...selectedTraits, trait]);
-                    });
-                    // break;
-                }
-            }, []);
-
-    if (selectedTraits.length > 0) {
-        return (
-            <div>
-                <h4>Traits:</h4>
-                {selectedTraits.map((trait) => 
-                    <div key={trait.index}>
-                    <p>{trait.name}</p>
-                    <p>{trait.desc}</p>
-                    </div>
-                )}
-            </div>
-        )
-    } else {
-        return (
-            <div>
-            </div>
-        )
+  useEffect(() => {
+    for (const trait of traits) {
+      fetchTrait(trait.index).then((trait) => {
+        console.log("after .then: " + JSON.stringify(trait));
+        setSelectedTraits([...selectedTraits, trait]);
+      });
+      // break;
     }
+  }, []);
+
+  if (selectedTraits.length > 0) {
+    return (
+      <div>
+        <h4>Traits:</h4>
+        {selectedTraits.map((trait) => (
+          <div key={trait.index}>
+            <p>{trait.name}</p>
+            <p>{trait.desc}</p>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 }
